2a2d461a0324a75b7c4db5478fbf0a14d917a354
Jonathan Castro <jonatancastro1@gmail.com>
2017-12-04 09:04:44 -0500

    Nuevos parques.

--- app/Http/Controllers/JornadaController.php
+++ app/Http/Controllers/JornadaController.php
@@ -151,6 +151,6 @@ class JornadaController extends Controller
 	}
 
 	private function parquesHabilitados() {
-		return Parque::whereIn('Id', [8585, 9478, 9989, 9936, 15565, 10721])->get();
+		return Parque::whereIn('Id', [8585, 9478, 9989, 9936, 15565, 10721, 10765, 15431])->get();
 	}
 }
